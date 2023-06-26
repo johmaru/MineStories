c18488a3c6228ebb7eaf1780419c18341b267a36
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-06-27 03:11:28 +0900

    ルート構成の変更

---
+++ src/main/java/johma/minestories/MineStories.java
@@ -0,0 +1,62 @@
+package johma.minestories;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MineStories.MODID)
+public class MineStories {
+
+    public static final String MODID = "minestories";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public MineStories(){
+
+        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        startingMsg();
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void setup(FMLCommonSetupEvent event){
+
+    }
+
+    private void startingMsg(){
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
